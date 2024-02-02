f9a9c47ac9b248ee16bb9f1ab51885c33be2c76f
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-02-02 17:09:40 -0400

    Update navigation structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native';
-import Router from './app/src/routes/Router';
+import styles from './src/styles';
+import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
-import {styles} from './app/src/styles';
+import AuthStack from './src/navigation/AuthStack';
+import MainStack from './src/navigation/MainStack';
+import LoadingScreen from './src/screens/LoadingScreen';
+import {useColorScheme} from 'react-native';
+import {darkTheme, lightTheme} from './src/styles/themes';
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
+}
 
 function App(): React.JSX.Element {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Router />
+      <NavigationContainer
+        theme={colorScheme === 'dark' ? darkTheme : lightTheme}>
+        {isLoading ? (
+          <LoadingScreen />
+        ) : isAuthenticated ? (
+          <MainStack />
+        ) : (
+          <AuthStack />
+        )}
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
