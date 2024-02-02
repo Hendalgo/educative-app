c6f5f864b48a01b3a7c0fa93d391b8b5d68799f5
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-02-02 19:38:55 -0400

    Add config to imports with babel and react-native-modalize dependency

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins:[
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ["."],
+        alias: {
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@screens": "./src/screens",
+          "@utils": "./src/utils",
+          "@services": "./src/services",
+          "@config": "./src/config",
+          "@navigations": "./src/navigations",
+          "@styles": "./src/styles",
+        }
+      }
+    ]
+  ]
 };
