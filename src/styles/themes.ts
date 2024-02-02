ae4b00b487bbcef2c64a023bd335a90f4c338b91
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-02-02 17:10:02 -0400

    Add styles and themes to application

---
+++ src/styles/themes.ts
@@ -0,0 +1,51 @@
+import {DefaultTheme} from '@react-navigation/native';
+
+export const lightTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#006DEC',
+    primary1600: '#ECF4FD',
+    primary1200: '#A1C9F7',
+    primary800: '#569EF0',
+    primary400: '#1474E1',
+    primary000: '#0D4D98',
+    primaryDark400: '#07274B',
+    primaryDark800: '#020A13',
+
+    neutral000: '#FFFFFF',
+    neutral300: '#EBEBEB',
+    neutral600: '#CCCCCC',
+    neutral900: '#ADADAD',
+    neutral1200: '#8F8F8F',
+    neutral1500: '#707070',
+    neutral1800: '#020A13',
+
+    black: '#191919',
+  },
+};
+
+export const darkTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#006DEC',
+    primary1600: '#ECF4FD',
+    primary1200: '#A1C9F7',
+    primary800: '#569EF0',
+    primary400: '#1474E1',
+    primary000: '#0D4D98',
+    primaryDark400: '#07274B',
+    primaryDark800: '#020A13',
+
+    neutral000: '#FFFFFF',
+    neutral300: '#EBEBEB',
+    neutral600: '#CCCCCC',
+    neutral900: '#ADADAD',
+    neutral1200: '#8F8F8F',
+    neutral1500: '#707070',
+    neutral1800: '#020A13',
+
+    black: '#191919',
+  },
+};
