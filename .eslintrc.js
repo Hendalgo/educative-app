bbcb792a5de51b75fa59e1169c0b4f1b316eb0d4
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-02-03 01:46:17 -0400

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  rules: {
-    'prettier/prettier': 0,
-    'no-unused-vars': 0,
-    'no-undef': 0,
-    'react-native/no-inline-styles': 0,
-  },
 };
