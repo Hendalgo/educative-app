8996ab1e557cfba5b7260187213053f8f79e21e1
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-02-03 20:51:41 -0400

    Add useKeyboardStatus hook

---
+++ src/hooks/useKeyboardStatus.ts
@@ -0,0 +1,28 @@
+import{ useState, useEffect } from 'react';
+import { Keyboard } from 'react-native';
+
+export const useKeyboardStatus = ():boolean => {
+  const [keyboardStatus, setKeyboardStatus] = useState<boolean>(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardStatus(true);
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardStatus(false);
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  return keyboardStatus;
+};
